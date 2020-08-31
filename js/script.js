458b6f0703a273b9cfafa6b8022bac0b9c2d0d1f
sergey.ar10n@gmail.com <31yjdsq07gfhjkm82>
2020-08-31 19:43:57 +0300

    Adding SCSS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+const mobileMenuBtn = document.querySelector(".mobile-navigation-button");
 const mobileMenu = document.querySelector(".mobile-menu");
-const answerArrows = document.querySelectorAll(".arrow");
+const answerArrows = document.querySelectorAll(".questions-list-item__arrow");
 
 mobileMenuBtn.addEventListener("click", () => {
     if (mobileMenu.classList.contains("show")) {
@@ -12,15 +12,15 @@ mobileMenuBtn.addEventListener("click", () => {
 
 for (let item of answerArrows) {
     item.addEventListener("click", () => {
-        let answerText = document.querySelector(`.answer.${item.classList[1]}`)
-        let question = document.querySelector(`.question-text.${item.classList[1]}`);
-        if (answerText.classList.contains("answer-show")) {
-            answerText.classList.remove("answer-show");
+        let answerText = document.querySelector(`.questions-list-item__answer.${item.classList[1]}`)
+        let question = document.querySelector(`.questions-list-item__text.${item.classList[1]}`);
+        if (answerText.classList.contains("questions-list-item__answer__show")) {
+            answerText.classList.remove("questions-list-item__answer__show");
             item.style.transform = "initial";
             question.style.color = "initial";
             question.style.background = "initial";
         } else {
-            answerText.classList.add("answer-show");
+            answerText.classList.add("questions-list-item__answer__show");
             item.style.transform = "rotate(180deg)";
             question.style.color = "#61A199";
             question.style.background = "rgba(97, 161, 153, 0.15)";
